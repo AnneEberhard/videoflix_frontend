43002d24a12ff38da0b4411fdd1a69c89ccdb4b1
AnneEberhard <anne.eberhard@gmx.net>
2024-03-26 10:10:02 +0100

    added authservice and environment

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/shared/services/auth.service';
+
 
 @Component({
     selector: 'app-login',
@@ -7,7 +9,7 @@ import { Router } from '@angular/router';
     styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-    constructor(private router: Router) { }
+    constructor(private router: Router, private authservice: AuthService) { }
 
 login() {
     this.router.navigate(['/main']);
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://example.com/api', // Hier kommt die Basis-URL für dein Produktions-Django-Backend
+  };
+  
