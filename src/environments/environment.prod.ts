e75a9c57c9e2f9640cc5bffd4b97cd2ff59bde89
AnneEberhard <anne.eberhard@gmx.net>
2024-04-08 18:34:58 +0200

    added file replacement in angular json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: 'https://example.com/api', // Hier kommt die Basis-URL für dein Produktions-Django-Backend
+    baseUrl: 'http://localhost:8000',
   };
   
