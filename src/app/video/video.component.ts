24aff9fbadf6d318fe0141fa301ab25f957bbc37
AnneEberhard <anne.eberhard@gmx.net>
2024-04-25 11:13:35 +0200

    added video style and logic

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
-  styleUrl: './video.component.scss'
+  styleUrls: ['./video.component.scss']
 })
-export class VideoComponent {
+export class VideoComponent implements OnInit {
+  videoUrl: string ='';
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    this.videoUrl = history.state.videoUrl;
+  }
+
+  goBack(): void {
+    this.router.navigate(['/main']);
+  }
 
 }
